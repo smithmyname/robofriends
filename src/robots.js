259934120a36cbfe482165531edb813b3176cac2
smithmyname <35342884+smithmyname@users.noreply.github.com>
2018-04-17 15:12:13 -0500

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Evie Hammond',
+	username: 'Evie',
+	email: 'evie@happyland.com'
+},
+{
+	id: 2,
+	name: 'Tyler Durden',
+	username: 'Tyler',
+	email: 'tyler@happyland.com' 
+},
+{
+	id: 3,
+	name: 'David Mills',
+	username: 'David',
+	email: 'david@happyland.com'
+},
+{
+	id: 4,
+	name: 'Thomas A. Anderson',
+	username: 'Neo',
+	email: 'neo@happyland.com'
+},
+{
+	id: 5,
+	name: 'Dante Alighieri',
+	username: 'Dante',
+	email: 'dante@happyland.com'
+},
+{
+	id: 6,
+	name: 'John Milton',
+	username: 'John',
+	email: 'john@happyland.com'
+},
+
+]
